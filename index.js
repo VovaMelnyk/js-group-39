66ab5c1d34de16f9f86db76eb7b4327bbf795d69
Vova Melnyk <hagmen4339@gmail.com>
2021-08-30 22:21:52 +0300

    finish lesson 1

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
-// "use strict";
 // x = 3.14;
+// console.log(`variable x`, x);
 // let x = 010;
+// console.log(`x`, x);
 // let public = 1500;
+// const apples = 47;
+// const grapes = 135;
+// debugger;
+
+// let price;
+// const percent = 12;
+// console.log(`price`, price);
+
+// const a = 5;
+// const b = "5";
+
+// console.log(a == b); // true
+// console.log(a === b); // false
+
+// '25px'
+
+// console.log("яблоки", apples);
+// console.log("виноград", grapes);
 
 // Выведи на экран общее количество яблок и винограда. Разницу яблок и винограда.
 
 // const apples = 47;
-// debugger;
 // const grapes = 135;
-// debugger;
-// const total = ;
-// console.log(total)
-// const diff = ;
-// console.log(diff)
+// const total = apples + grapes;
+// console.log(total);
+// const diff = apples - grapes;
+// console.log(diff);
 
 // Замени выражение переопределения комбинированным оператором `+=`.
 
 // let students = 100;
-// students = students + 50;
+// // students = students + 50;
+// students += 50;
 // console.log(students);
 
 // Разбери приоритет операторов в инструкции присвоения значения переменной
@@ -31,14 +49,27 @@
 // `Math.round()`. Проверь что будет в консоли при значениях `27.3` и `27.9`.
 
 // const value = 27.5;
+// console.log("floor", Math.floor(value));
+// console.log("ceil", Math.ceil(value));
+// console.log("round", Math.round(value));
+// const user1 = 100;
+// const user2 = 123;
+// const user3 = 34;
+// const maxValue = Math.max(user1, user2, user3);
 
 // Составь фразу с помощью шаблонных строк `A has B bots in stock`, где A, B -
 // переменные вставленные в строку.
 
-// const companyName = 'Cyberdyne Systems';
+// const companyName = "Cyberdyne Systems";
 // const repairBots = 150;
 // const defenceBots = 50;
-// const message = ``;
+
+// const message = `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// const userName = "Bob";
+// const budget = 1500000;
+// const filmName = "Batman";
+// const date = "23.05.2020";
+// const filmMessage = `Привет ${userName}, сегодня вишло ${filmName} , с бюджетом ${budget} и дата проката ${date} `;
 // console.log(message); // "Cyberdyne Systems has 200 bots in stock"
 
 // Каким будет результат выражений?
@@ -49,7 +80,10 @@
 
 // console.log('2' > '12');
 
-// console.log('2' < '12');
+// console.log("12".charCodeAt(0));
+// console.log("2".charCodeAt(0));
+
+// console.log("2" < "12");
 
 // console.log('4' == 4);
 
@@ -57,9 +91,13 @@
 
 // console.log('false' === false);
 
-// console.log(1 == true);
+// console.log(1 == true); // true
 
-// console.log(1 === true);
+// console.log( 1 == 1)//
+
+// console.log(1 === true); // false
+
+// console.log(1 === 1); // true
 
 // console.log('0' == false);
 
@@ -67,7 +105,7 @@
 
 // console.log('Papaya' < 'papaya');
 
-// console.log('Papaya' === 'papaya');
+// console.log("Papaya" === "papaya");
 
 // console.log(undefined == null);
 
@@ -89,11 +127,26 @@
 
 // console.log(true || false || 7);
 
-// console.log(null || 2 || undefined);
+// console.log(null || " " || undefined);
 
 // console.log((1 && null && 2) > 0);
 
+// console.log(null > 0)
+
 // console.log(null || (2 && 3) || 4);
+
+// console.log(null || 3 || 4);
+
+// console.log("ghbdsn!" && 25 && null);
+ 
+// 0
+// ''
+// false
+// NaN
+// null
+// undefined
+// -0
+
 
 // Отрефактори код так, чтобы в переменную `value` присваивалось значение
 // переменной `incomingValue`, если оно не равно `undefined` или `null`. В
