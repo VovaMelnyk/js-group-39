f103d3855cb6ed71da5998e4469f174ebf23a996
Vova Melnyk <hagmen4339@gmail.com>
2021-09-06 21:53:49 +0300

    finis lesson 3

--- index.js
+++ index.js
@@ -56,22 +56,102 @@
 // Задача 4 - Массивы и циклы
 // Напиши скрипт который выводит в консоль имя и телефонный номер пользователя. В переменных names и phones хранятся строки имен и телефонных номеров, разделенные запятыми. Порядковый номер имен и телефонов в строках указывают на соответствие. Количество имен и телефонов гарантированно одинаковое.
 
-// const names = 'Jacob,William,Solomon,Artemis';
-// const phones = '89001234567,89001112233,890055566377,890055566300';
+// const names = "Jacob,William,Solomon,Artemis";
+// const phones = "89001234567,89001112233,890055566377,890055566300";
+
+// const namesArray = names.split(",");
+// const phonesArray = phones.split(",");
+// console.log(`namesArray`, namesArray);
+// console.log(`phonesArray`, phonesArray);
+
+// for (let i = 0; i < namesArray.length; i += 1) {
+//     console.log(`${namesArray[i]} - ${phonesArray[i]}`);
+// }
+
 // Задача 5 - Массивы и строки
 // Напиши скрипт который выводит в консоль все слова строки кроме первого и последнего. Результирующая строка не должна начинаться или заканчиваться пробельным символом. Скрипт должен работать для любой строки.
 
-// const string = 'Welcome to the future';
+// const string = "Welcome to the future";
+// const array = string.split(" ");
+// array.shift();
+// array.pop();
+// console.log(`array`, array);
+// const newString = array.join(" ");
+// console.log(newString);
+
+// let newArray = [];
+// for (let i = 0; i < array.length; i++) {
+//   if (i === 0 || i === array.length - 1) {
+//     continue;
+//   }
+
+//   const word = array[i];
+
+//   newArray.push(word);
+// }
+
+// console.log(`newArray`, newArray);
+// const newString = newArray.join(" ");
+// console.log(newString);
+
 // Задача 6 - Массивы и строки
 // Напиши скрипт который «разворачивает» строку (обратный порядок букв) и выводит ее в консоль.
 
-// const string = 'Welcome to the future';
+// const string = "Welcome to the future";
+
+// const array = string.split(" ");
+// const reversedArray = array.reverse();
+// const reversedString = reversedArray.join(" ");
+// console.log(`array`, array);
+// console.log(`reversedArray`, reversedArray);
+// console.log("reversedString", reversedString);
+
+// const array = string.split(" ");
+// console.log(`array`, array);
+
+// const newArray = [];
+
+// for (let i = array.length - 1; i >= 0; i--) {
+//   const word = array[i];
+//   newArray.push(word);
+// }
+
+// const reversedString = newArray.join(" ");
+// console.log(`newArray`, newArray);
+// console.log("reversedString", reversedString);
+
+// const result = string === reversedString;
+
+// console.log(`result`, result);
 
 // Задача 7 - Поиск элемента
 // Напиши скрипт поиска самого маленького числа в массиве. Код должен работать для любого массива чисел. Используй цикл для решения задачи.
 
-// const numbers = [2, 17, 94, 1, 23, 37];
-// let min;
+// const numbers = [1, 17, 94, 2, 23, 37, -1];
+
+// let min = numbers[0];
+
+// for (let number of numbers) {
+//   if (min > number) {
+//     min = number;
+//   }
+// }
+
+// min = 2; min = 2; min 2; min = 2; min = 1; min = 1
+// number = 2; number = 17; number = 94; number = 1; number = 23; number = 37
+
 // console.log(min); // 1
 
+// console.log(`numbers`, Math.min(...numbers));
+
 // Задача 8 - использование includes
+
+// const emails = ["user@gmail.com", "admin@gmail.com", "student@gmail.com"];
+
+// const userMail = prompt("Enter your email");
+
+// if (emails.includes(userMail)) {
+//   console.log("Login");
+// } else {
+//   console.log("Wrong email");
+// }
