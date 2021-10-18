b81415ffaa2d4ef6f2c08c14d251e2f578b0275c
Alexander Repeta <alexander.repeta@gmail.com>
2021-10-18 14:46:12 +0300

    add examples

--- js/app.js
+++ js/app.js
@@ -1,19 +1,38 @@
-const form = document.querySelector('.note-editor');
-const notesContainer = document.querySelector('.notes');
+const resources = [
+  { title: 'Some article', type: 'article', href: 'habr.com' },
+  { title: 'Some video', type: 'video', href: 'vimeo.com' },
+  { title: 'Some chart', type: 'chart', href: 'my-chart.com' },
+  { title: 'Some pdf', type: 'pdf', href: 'my-doc.pdf' },
+];
 
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  createNote(form.elements.text.value);
-  form.reset();
+const container = document.querySelector('.resources');
+
+const cardsMarkup = resources
+  .map(({ title, type, href }) => {
+    return `
+      <a class="card" data-type="${type}" href="${href}" target="_blank">${title}</a>
+    `;
+  })
+  .join('');
+
+container.insertAdjacentHTML('beforeend', cardsMarkup);
+
+container.addEventListener('click', e => {
+  const resourceType = e.target.dataset.type;
+  if (resourceType !== 'article') {
+    e.preventDefault();
+  }
+  switch (resourceType) {
+    case 'video':
+      console.log('Open video player');
+      break;
+
+    case 'chart':
+      console.log('Show chart');
+      break;
+
+    case 'pdf':
+      console.log('Open pdf viewer');
+      break;
+  }
 });
-
-function createNote(text) {
-  const noteEl = document.createElement('div');
-  noteEl.classList.add('note');
-  noteEl.textContent = text;
-  const delBtn = document.createElement('button');
-  delBtn.type = 'button';
-  delBtn.textContent = 'Удалить';
-  noteEl.append(delBtn);
-  notesContainer.append(noteEl);
-}
